1d5b3f32d88c95bb3eca19b5c3fd87e616ff6e2f
Chad Page <Chad.Page@gmail.com>
2015-02-08 12:42:25 -0800

    finished MVP version.  listens on ports 4000-4015, select which one to pipe to stdout by pressing 0..9, aA..aF.

--- projects/acatcher/acatcher.cxx
+++ projects/acatcher/acatcher.cxx
@@ -24,12 +24,14 @@ THE SOFTWARE.
 
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <strings.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <termios.h>
 
 using namespace std;
 
@@ -48,17 +50,20 @@ struct sockin {
 		bzero(&serv_addr, sizeof(serv_addr));
 		bzero(&cli_addr, sizeof(cli_addr));
 	}
+	
+	void handle(unsigned char *data, int len, bool have_stdout) {
+	}
 }; 
 
 struct audio_sockin : public sockin {
 	int leftover;
 
-	uint16_t buf[ABUFLEN];
-	int bufsize;
+//	uint16_t buf[ABUFLEN];
+//	int bufsize;
 
 	audio_sockin() : sockin() {
 		leftover = -1;
-		bufsize = 0;
+		//bufsize = 0;
 	}
 	
 	void handle(unsigned char *data, int len, bool have_stdout) {
@@ -70,20 +75,76 @@ struct audio_sockin : public sockin {
 			write(1, data, len);
 		}
 	}
+
+	// new connection: better not have an odd # output for audio!
+	void newconn() {
+		leftover = -1;
+	}
 };
 
 audio_sockin s[MAX];
 
 unsigned char buf[BUFLEN];
 
+struct termios oldtermios;
+
+void sigcatch(int sig)
+{
+	tcsetattr(0, TCSAFLUSH, &oldtermios);
+	_exit(0);
+} 
+
 int main(void)
 {
+	int main_rv = 0;
+	int cur_listener = 0;
+
+	// catch signals
+	if (((int)signal(SIGINT,sigcatch) < 0) ||
+	    ((int)signal(SIGQUIT,sigcatch) < 0) ||
+	    ((int)signal(SIGTERM,sigcatch) < 0)) {
+		cerr << "Couldn't set up signal catching.  huh?\n";
+		return(1);
+	}
+
+	// set keyboard to raw mode + unblocking 
+	struct termios newtermios;
+
+	if(tcgetattr(0, &oldtermios) < 0)
+		return(-1);
+
+	newtermios = oldtermios;
+
+	newtermios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	newtermios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	newtermios.c_cflag &= ~(CSIZE | PARENB);
+
+	newtermios.c_cflag |= CS8;
+
+	newtermios.c_oflag &= ~(OPOST);
+
+	newtermios.c_cc[VMIN] = 1;
+	newtermios.c_cc[VTIME] = 0;
+
+	if(tcsetattr(0, TCSAFLUSH, &newtermios) < 0)
+		return(-1);
+						
+	int opts = fcntl(0, F_GETFL);
+	if (opts < 0) {
+		cerr << "HUH?  fcntl failed\n";
+		goto err_exit;
+	} 
+	if (fcntl(0, F_SETFL, opts | O_NONBLOCK) < 0) {
+		cerr << "HUH?  fcntl(F_SETFL) failed\n";
+		goto err_exit;
+	} 
+
 	// init listening sockets
 	for (int i = 0; i < MAX; i++) {
 		s[i].listener_fd = socket(AF_INET, SOCK_STREAM, 0);
 		if (s[i].listener_fd < 0) {
 			cerr << "Couldn't open a listener socket.  Weird.\n";
-			return(1);
+			goto err_exit;
 		}
 
 		s[i].serv_addr.sin_family = AF_INET;
@@ -92,7 +153,7 @@ int main(void)
 
 		if (bind(s[i].listener_fd, (struct sockaddr *)&s[i].serv_addr, sizeof(s[i].serv_addr))) {
 			cerr << "ERROR: Couldn't bind to port #" << baseport + i << endl;
-			return(1);
+			goto err_exit;
 		}
 
 		listen(s[i].listener_fd, 1);
@@ -113,9 +174,11 @@ int main(void)
 		FD_ZERO(&writefds);
 		FD_ZERO(&exceptfds);
 
+		FD_SET(0, &readfds);
+
 		for (int i = 0; i < MAX; i++) {
 			if (s[i].listener_fd >= 0) {
-				FD_SET(s[i].listener_fd + 1, &readfds);	
+				FD_SET(s[i].listener_fd, &readfds);	
 				if (s[i].listener_fd >= topfd) topfd = s[i].listener_fd + 1; 
 			}
 			if (s[i].data_fd >= 0) {
@@ -127,7 +190,34 @@ int main(void)
 		rv = pselect(topfd, &readfds, &writefds, &exceptfds, &t, NULL);
 		if (rv == -1 && errno != EINTR) { 
 			cerr << "ERROR: select failed\n";
-			return(1);
+			goto err_exit;
+		}
+
+		if ((rv > 0) && FD_ISSET(0, &readfds)) {
+			unsigned char c;
+
+			int rv = read(0, &c, 1);
+
+			if (rv == 0) goto good_exit; 
+
+			if (rv == 1) {
+				int old_listener = cur_listener;
+
+				cerr << "Got char " << (int)c << "\r\n"; 
+				if (c == 'q') goto good_exit;
+				if ((c >= '0') && (c <= '9')) {
+					cur_listener = c - '0';
+				}
+				if ((c >= 'a') && (c <= 'f')) {
+					cur_listener = c + 10 - 'a';
+				}
+				if ((c >= 'A') && (c <= 'F')) {
+					cur_listener = c + 10 - 'A';
+				}
+				if (old_listener != cur_listener) {
+					cerr << "Now listening to #" << cur_listener << "\r\n";
+				}
+			}
 		}
 
 		for (int i = 0; (rv > 0) && (i < MAX); i++) {
@@ -137,20 +227,21 @@ int main(void)
 				int newfd = accept(s[i].listener_fd, (struct sockaddr *)&s[i].cli_addr, &len);
 				if (newfd >= 0) {
 					if (s[i].data_fd < 0) {
-						int opts;
+						int opts = fcntl(newfd, F_GETFL);
 
-						opts = fcntl(newfd, F_GETFL);
 						if (opts < 0) {
 							cerr << "HUH?  fcntl failed\n";
-							return(1);	
+							goto err_exit;
 						} 
+
 						if (fcntl(newfd, F_SETFL, opts | O_NONBLOCK) < 0) {
 							cerr << "HUH?  fcntl(F_SETFL) failed\n";
-							return(1);	
+							goto err_exit;
 						} 
 
 						s[i].data_fd = newfd;
-						cerr << i << ' ' << s[i].data_fd << endl;
+						cerr << "New connection on " << i << "\r\n";
+						s[i].newconn();
 					} else {
 						cerr << "HUH?  new connection on socket # " << i << endl;
 					} 
@@ -166,7 +257,7 @@ int main(void)
 					s[i].data_fd = -1;
 					listen(s[i].listener_fd, 1);
 				} else {
-					handle(buf, rv, true);
+					s[i].handle(buf, rv, (i == cur_listener));
 //					rv = write(1, buf, rv);
 				}
 			}
@@ -174,7 +265,13 @@ int main(void)
 	};
 
 
-	return 0;
+	err_exit:
+		tcsetattr(0, TCSAFLUSH, &oldtermios);
+		return(1);
+
+	good_exit:
+		tcsetattr(0, TCSAFLUSH, &oldtermios);
+		return(0);
 }
 
 
